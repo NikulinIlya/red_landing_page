13ff103328bc816822495e97b4116adced65ad56
Ilya <ilia.nikulin@gmail.com>
2018-07-13 18:11:30 +0300

    email

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 /*
  * группа маршрутов пользовательской части
  */
-Route::group(['middleware'=>'web'], function () {
+Route::group([], function () {
     Route::match(['get', 'post'], '/', ['uses'=>'IndexController@execute', 'as'=>'home']);
     Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as'=>'page']);
 
