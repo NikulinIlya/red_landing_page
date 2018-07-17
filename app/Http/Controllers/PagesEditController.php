c896d5634c81975d88eb3570d92e0ea428122cb3
Ilya <ilia.nikulin@gmail.com>
2018-07-17 16:12:44 +0300

    pages delete

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -10,6 +10,12 @@ class PagesEditController extends Controller
 {
     public function execute(Page $page, Request $request) {
 
+        if($request->isMethod('delete')) {
+            $page->delete();
+            return redirect('admin')->with('status', 'Страница удалена');
+        }
+
+
         if($request->isMethod('post')) {
 
             $input = $request->except('_token');
